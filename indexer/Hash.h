b9da72630acf44c867451d023afe3480b1fe7196
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-01-02 12:45:03 +0800

    cleanup: Remove HashConsed type for now.

--- indexer/Hash.h
+++ indexer/Hash.h
@@ -7,8 +7,6 @@
 #include "absl/hash/hash.h"
 #include "wyhash.h"
 
-#include "llvm/ADT/DenseMapInfo.h"
-
 namespace scip_clang {
 
 struct HashValue {
@@ -30,41 +28,6 @@ struct HashValue {
   }
   friend bool operator<(const HashValue &lhs, const HashValue &rhs) {
     return lhs.rawValue < rhs.rawValue;
-  }
-};
-
-} // namespace scip_clang
-
-namespace scip_clang {
-
-template <typename T> class HashConsed {
-  uint64_t value;
-  T _data;
-
-public:
-  HashConsed() {
-    return HashConsed(T());
-  };
-  HashConsed(HashConsed<T> &&) = default;
-  HashConsed &operator=(HashConsed<T> &&) = default;
-  HashConsed(const HashConsed<T> &) = default;
-  HashConsed &operator=(const HashConsed<T> &) = default;
-
-  HashConsed(T &&data) : value(absl::HashOf(data)), _data(data) {}
-
-  template <typename H> friend H AbslHashValue(H h, const HashConsed<T> &hc) {
-    return H::combine(std::move(h), hc.value);
-  }
-
-  friend bool operator==(const HashConsed<T> &lhs, const HashConsed<T> &rhs) {
-    return lhs.value == rhs.value && lhs.data() == rhs.data();
-  }
-  friend bool operator!=(const HashConsed<T> &lhs, const HashConsed<T> &rhs) {
-    return !(lhs == rhs);
-  }
-
-  const T &data() const {
-    return this->_data;
   }
 };
 
