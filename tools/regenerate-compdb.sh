d1eeb871aecbb129bd8c477baa4be91f8b388564
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-02-14 18:21:50 +0800

    fix: Remove --config=dev when generating compdb (#74)

--- tools/regenerate-compdb.sh
+++ tools/regenerate-compdb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-bazel build //tools:compdb --spawn_strategy=local --config=dev
+# For some reason, using --config=dev causes clangd errors inside Abseil
+# on macOS. So use the default config instead.
+bazel build //tools:compdb --spawn_strategy=local
 
 PROJECT_ROOT="$(dirname "${BASH_SOURCE[0]}")/.."
 
