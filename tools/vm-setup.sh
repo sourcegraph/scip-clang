b8286d07fa0555a9bcf92bb044d876014ddbebed
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-02-15 08:43:09 +0800

    fix: Update VM setup script for toolchain changes (#79)

--- tools/vm-setup.sh
+++ tools/vm-setup.sh
@@ -1,10 +1,6 @@
 # NOTE: Keep in sync with .buildkite/pipeline.yml
 sudo apt update
-curl -L https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-add-apt-repository 'deb http://apt.llvm.org/focal/  llvm-toolchain-focal-15 main'
-sudo apt install clang-15 libtinfo-dev unzip ripgrep fd-find
-export CC=clang-15
-export CXX=clang++-15
+sudo apt install g++ unzip ripgrep fd-find
 wget -L https://github.com/bazelbuild/bazelisk/releases/download/v1.15.0/bazelisk-linux-amd64 -O bazel
 chmod +x bazel
 sudo mv bazel /usr/local/bin/bazel
