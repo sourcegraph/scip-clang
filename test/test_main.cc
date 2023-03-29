0df612ed2591af2c190e793db9c20f12b1062397
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-03-29 16:21:38 +0900

    test: Run index tests in deterministic mode (#155)

--- test/test_main.cc
+++ test/test_main.cc
@@ -420,6 +420,7 @@ TEST_CASE("INDEX") {
         args.push_back("--receive-timeout-seconds=60");
         args.push_back(fmt::format("--driver-id=index-{}",
                                    test::globalCliOptions.testName));
+        args.push_back("--deterministic");
         boost::process::child driver(
             args, boost::process::start_dir(rootInSourceDir.asStringRef()),
             boost::process::std_out > stdout, boost::process::std_err > stderr);
