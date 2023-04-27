aa3206f51852eac77241944b7a9da079b9197692
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-04-27 08:58:48 +0800

    cleanup: Re-arrange command-line flags a bit

--- indexer/main.cc
+++ indexer/main.cc
@@ -27,92 +27,107 @@ static scip_clang::CliOptions parseArguments(int argc, char *argv[]) {
   cliOptions.numWorkers = std::thread::hardware_concurrency();
 
   cxxopts::Options parser("scip-clang", "SCIP indexer for C-based languages");
+
+  std::string defaultGroup = "";
   // clang-format off
-  parser.add_options("")(
+  parser.add_options(defaultGroup)(
     "compdb-path",
     "Path to JSON compilation database",
     cxxopts::value<std::string>(cliOptions.compdbPath)->default_value("compile_commands.json"));
-  parser.add_options("")(
+  parser.add_options(defaultGroup)(
     "index-output-path",
     "Path to write the SCIP index to",
     cxxopts::value<std::string>(cliOptions.indexOutputPath)->default_value("index.scip"));
-  parser.add_options("")(
+  parser.add_options(defaultGroup)(
     "j,jobs",
     fmt::format(
       "Upper bound for number of indexing processes to run in parallel (default: NCPUs = {})",
       cliOptions.numWorkers),
     cxxopts::value<uint32_t>(cliOptions.numWorkers));
-  parser.add_options("")(
+  parser.add_options(defaultGroup)(
     "log-level",
     "One of 'debug', 'info', 'warning' or 'error'",
     cxxopts::value<std::string>()->default_value("info"));
-  parser.add_options("")(
-    "temporary-output-dir",
-    "Store temporary files under a specific directory instead of using system APIs."
-    "If set, this directory will not be deleted after indexing is complete.",
-    cxxopts::value<std::string>(cliOptions.temporaryOutputDir));
-  parser.add_options("")(
+  parser.add_options(defaultGroup)(
     "show-compiler-diagnostics",
     "Show Clang diagnostics triggered when running semantic analysis."
     " Useful for debugging issues related to missing headers.",
     cxxopts::value<bool>(cliOptions.showCompilerDiagnostics));
-  parser.add_options("")("version", "Show the version", cxxopts::value<bool>());
-  parser.add_options("")("h,help", "Show help text", cxxopts::value<bool>());
-  parser.add_options("Advanced")(
+  parser.add_options(defaultGroup)("version", "Show the version and exit", cxxopts::value<bool>());
+  parser.add_options(defaultGroup)("h,help", "Show default help text", cxxopts::value<bool>());
+  parser.add_options(defaultGroup)(
+    "help-all",
+    "Show all command-line flags, including unstable development flags and internal flags",
+    cxxopts::value<bool>());
+  // End of common options
+  parser.add_options("Limits")(
     "ipc-size-hint-bytes",
     "A size hint for how space is available for IPC (per worker)."
     " The actual space consumption may exceed this hint by ~15%.",
     cxxopts::value<size_t>(cliOptions.ipcSizeHintBytes)->default_value("2000000"));
     // ^ The default value of 2MB should be enough for typical invocations.
-  parser.add_options("Advanced")(
-    "print-statistics-path",
-    "Print indexing related statistics in JSON format."
-    " Caution: Timing information for individual TUs should not be compared"
-    " directly across runs, as non-determinism may affect the number of files"
-    " skipped by individual indexing jobs.",
-    cxxopts::value<std::string>(cliOptions.statsFilePath));
-  parser.add_options("Advanced")(
+  parser.add_options("Limits")(
     "receive-timeout-seconds",
     "How long should the driver wait for a worker before marking it as timed out?",
     cxxopts::value<uint32_t>()->default_value("300"));
-  parser.add_options("Advanced")(
+  parser.add_options("Debugging")(
+    "worker-mode",
+    "[worker-only] Spawn an indexing worker instead of invoking the driver directly."
+    " One of 'ipc', 'compdb' or 'testing'."
+    " Useful for iterating on a fix for a small compilation database.",
+    cxxopts::value<std::string>(cliOptions.workerMode)->default_value(""));
+  parser.add_options("Debugging")(
     "deterministic",
     "Try to run everything in a deterministic fashion as much as possible."
     " Does not support deterministic work scheduling yet."
     " When using this flag, explicitly pass --temporary-output-dir to fix paths too.",
     cxxopts::value<bool>(cliOptions.deterministic));
-  parser.add_options("Advanced")(
+  parser.add_options("Debugging")(
+    "temporary-output-dir",
+    "Store temporary files under a specific directory instead of using system APIs."
+    "If set, this directory will not be deleted after indexing is complete.",
+    cxxopts::value<std::string>(cliOptions.temporaryOutputDir));
+  parser.add_options("Debugging")(
+    "supplementary-output-dir",
+    "Path to directory for recording supplementary outputs, such as various log files.",
+    cxxopts::value<std::string>(cliOptions.supplementaryOutputDir)->default_value("scip-clang-supplementary-output"));
+  parser.add_options("Debugging")(
     "preprocessor-record-history-filter",
     "Regex for identifying files for which textual descriptions of preprocessor"
     " effects should be recorded while computing transcripts, instead of"
     " only maintaining a running hash value. The effects are recorded in YAML"
     " format under --supplementary-output-dir.",
     cxxopts::value<std::string>(cliOptions.preprocessorRecordHistoryFilterRegex));
-  parser.add_options("Advanced")(
-    "supplementary-output-dir",
-    "Path to directory for recording supplementary outputs, such as various log files.",
-    cxxopts::value<std::string>(cliOptions.supplementaryOutputDir)->default_value("scip-clang-supplementary-output"));
-  parser.add_options("Advanced")(
-    "help-all",
-    "Show all command-line flags, including internal ones and ones for testing.",
-    cxxopts::value<bool>());
-  parser.add_options("Advanced")(
+  parser.add_options("Profiling")(
+    "print-statistics-path",
+    "Print indexing related statistics in JSON format."
+    " Caution: Timing information for individual TUs should not be compared"
+    " directly across runs, as non-determinism may affect the number of files"
+    " skipped by individual indexing jobs.",
+    cxxopts::value<std::string>(cliOptions.statsFilePath));
+  parser.add_options("Profiling")(
     "measure-statistics",
     "[worker-only] Measure various statistics related to indexing",
     cxxopts::value<bool>(cliOptions.measureStatistics));
-  parser.add_options("Internal")(
+  std::string testGroup = "scip-clang internal testing";
+  parser.add_options(testGroup)(
     "no-stack-trace",
-    "Skip printing the stack trace on crashes.",
+    "Skip printing the stack trace on crashes. Only meant for testing"
+    " to ensure deterministic output.",
     cxxopts::value<bool>(cliOptions.noStacktrace));
+  parser.add_options(testGroup)(
+    "force-worker-fault",
+    "One of 'crash', 'sleep' or 'spin'."
+    " Forces faulty behavior in a worker process instead of normal processing.",
+    cxxopts::value<std::string>(cliOptions.workerFault)->default_value(""));
+  parser.add_options(testGroup)(
+    "testing",
+    "Running for scip-clang internal tests.",
+    cxxopts::value<bool>());
   parser.add_options("Internal")(
     "preprocessor-history-log-path",
     "[worker-only] Path to log preprocessor history, if applicable.",
     cxxopts::value<std::string>(cliOptions.preprocessorHistoryLogPath));
-  parser.add_options("Internal")(
-    "worker-mode",
-    "[worker-only] Spawn an indexing worker instead of invoking the driver directly."
-    " One of 'ipc', 'compdb' or 'testing'.",
-    cxxopts::value<std::string>(cliOptions.workerMode)->default_value(""));
   parser.add_options("Internal")(
     "driver-id",
     "An opaque ID for the driver. Normally, this is only used by the driver "
@@ -123,15 +138,6 @@ static scip_clang::CliOptions parseArguments(int argc, char *argv[]) {
     "worker-id",
     "[worker-only] An opaque ID for the worker itself.",
     cxxopts::value<uint64_t>(cliOptions.workerId));
-  parser.add_options("Testing")(
-    "force-worker-fault",
-    "One of 'crash', 'sleep' or 'spin'."
-    " Forces faulty behavior in a worker process instead of normal processing.",
-    cxxopts::value<std::string>(cliOptions.workerFault)->default_value(""));
-  parser.add_options("Testing")(
-    "testing",
-    "Running for scip-clang internal tests.",
-    cxxopts::value<bool>());
 
   // TODO(def: flag-passthrough, issue: https://github.com/sourcegraph/scip-clang/issues/23)
   // Support passing through CLI flags to Clang, similar to --extra-arg in lsif-clang
@@ -143,7 +149,7 @@ static scip_clang::CliOptions parseArguments(int argc, char *argv[]) {
   cxxopts::ParseResult result = parser.parse(argc, argv);
 
   if (result.count("help") || result.count("h")) {
-    fmt::print("{}\n", parser.help({"", "Advanced"}));
+    fmt::print("{}\n", parser.help({defaultGroup}));
     std::exit(EXIT_SUCCESS);
   }
   if (result.count("help-all")) {
