6d713a3f45fcc3b4d017ac2e3e6a2b199b74721a
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-05-05 13:29:54 +0800

    fix: Emit defs/refs for function-local types

--- test/index/functions/template_body.snapshot.cc
+++ test/index/functions/template_body.snapshot.cc
@@ -4,15 +4,20 @@
   void f() {
 //     ^ definition [..] f(49f6e7a06ebc5aa8).
     struct C {
+//         ^ definition [..] f(49f6e7a06ebc5aa8).C#
       T field;
 //    ^ reference local 0
+//      ^^^^^ definition [..] f(49f6e7a06ebc5aa8).C#field.
   
       void g() {};
+//         ^ definition [..] f(49f6e7a06ebc5aa8).C#g(49f6e7a06ebc5aa8).
     };
   
     auto t = C().field;
 //       ^ definition local 1
+//           ^ reference [..] f(49f6e7a06ebc5aa8).C#
     C().g();
+//  ^ reference [..] f(49f6e7a06ebc5aa8).C#
   
     int x = 0;
 //      ^ definition local 2
