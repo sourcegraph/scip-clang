ae4d718e2b7c9ccf376424d317ecfc0f96dc5e2b
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-05-18 10:37:05 +0800

    fix: Treat all kinds of comments as doc comments (#325)

---
+++ test/index/docs/docs.snapshot.cc
@@ -0,0 +1,45 @@
+  // format-options: showDocs
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^ definition [..] `<file>/docs.cc`/
+//documentation
+//| File: docs.cc
+  
+  /// Dumhdurum!
+  enum class Apink {
+//           ^^^^^ definition [..] Apink#
+//           documentation
+//           | Dumhdurum!
+    /** Leader
+     * Lead Dancer */
+    Chorong,
+//  ^^^^^^^ definition [..] Apink#Chorong.
+//  documentation
+//  | Leader
+//  | Lead Dancer
+    // Vocalist
+    Bomi,
+//  ^^^^ definition [..] Apink#Bomi.
+//  documentation
+//  | Vocalist
+    Eunji,  // Main Vocalist
+//  ^^^^^ definition [..] Apink#Eunji.
+//  documentation
+//  | Main Vocalist
+//  | From Busan
+            // From Busan
+    /* Departed
+     * :( */
+    Naeun,
+//  ^^^^^ definition [..] Apink#Naeun.
+//  documentation
+//  | Departed
+//  | :(
+    Namjoo, /* Vocalist */
+//  ^^^^^^ definition [..] Apink#Namjoo.
+//  documentation
+//  | Vocalist
+    //! Maknae
+    Hayoung,
+//  ^^^^^^^ definition [..] Apink#Hayoung.
+//  documentation
+//  | Maknae
+  };
