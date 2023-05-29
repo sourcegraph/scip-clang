203e7a85da0b2a06597e527ef4d4f9847c6d868a
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-05-29 09:03:39 +0800

    fix: Add external symbols to forward decl resolver

--- test/index/crossrepo/external_symbols.snapshot.cc
+++ test/index/crossrepo/external_symbols.snapshot.cc
@@ -1,9 +1,6 @@
 // dep1 v1 $dep1/
 // documentation
 // | namespace dep1
-// [..] dep1/f(49f6e7a06ebc5aa8).
-// documentation
-// | No documentation available.
 // dep1 v1 $dep1/f(49f6e7a06ebc5aa8).
 // documentation
 // | No documentation available.
--- test/index/crossrepo/main.snapshot.cc
+++ test/index/crossrepo/main.snapshot.cc
@@ -6,6 +6,6 @@
 //    ^^^^ definition main v0 $main(afcd3168fc1188a4).
     dep1::f();
 //  ^^^^ reference dep1 v1 $dep1/
-//        ^ reference [..] dep1/f(49f6e7a06ebc5aa8).
+//        ^ reference dep1 v1 $dep1/f(49f6e7a06ebc5aa8).
     return 0;
   }
