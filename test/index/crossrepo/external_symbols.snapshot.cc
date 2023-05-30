3a422cad5c1f4b1bf806cc19cdffe2c24c8bb325
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-05-30 12:59:44 +0800

    test: Update snapshot output

--- test/index/crossrepo/external_symbols.snapshot.cc
+++ test/index/crossrepo/external_symbols.snapshot.cc
@@ -1,6 +1,6 @@
-// dep1 v1 $dep1/
+// dep1 v1$ dep1/
 // documentation
 // | namespace dep1
-// dep1 v1 $dep1/f(49f6e7a06ebc5aa8).
+// dep1 v1$ dep1/f(49f6e7a06ebc5aa8).
 // documentation
 // | No documentation available.
--- test/index/crossrepo/main.snapshot.cc
+++ test/index/crossrepo/main.snapshot.cc
@@ -1,11 +1,11 @@
   #include "external/dep1/dep1.h"
-//^ definition main v0 $`<file>/main.cc`/
-//         ^^^^^^^^^^^^^^^^^^^^^^ reference dep1 v1 $`<file>/dep1.h`/
+//^ definition main v0$ `<file>/main.cc`/
+//         ^^^^^^^^^^^^^^^^^^^^^^ reference dep1 v1$ `<file>/dep1.h`/
   
   int main(int, char **) {
-//    ^^^^ definition main v0 $main(afcd3168fc1188a4).
+//    ^^^^ definition main v0$ main(afcd3168fc1188a4).
     dep1::f();
-//  ^^^^ reference dep1 v1 $dep1/
-//        ^ reference dep1 v1 $dep1/f(49f6e7a06ebc5aa8).
+//  ^^^^ reference dep1 v1$ dep1/
+//        ^ reference dep1 v1$ dep1/f(49f6e7a06ebc5aa8).
     return 0;
   }
