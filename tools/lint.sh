96715033fc5a8d393391be55cfa393ce0b4a9726
Varun Gandhi <varun.gandhi@sourcegraph.com>
2024-03-01 15:22:40 +0800

    tools: Fix incorrect cache check in linting step

--- tools/lint.sh
+++ tools/lint.sh
@@ -3,9 +3,7 @@ set -euo pipefail
 
 PROJECT_ROOT="$(dirname "${BASH_SOURCE[0]}")/.."
 
-if [ ! -f "bazel-bin/third_party/actionlint" ]; then
-  bazel build //third_party:actionlint
-fi
+bazel build //third_party:actionlint
 
 (
   cd "$PROJECT_ROOT"
