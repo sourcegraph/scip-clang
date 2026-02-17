858ce0b409bb651f9ce08932de9b90bb8ad04f2a
Adam Chrapkowski <adam.chrapkowski@sourcegraph.com>
2026-02-17 17:52:18 +0100

    Applied review comments

--- tools/regenerate-compile-commands.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Generates compile_commands.json for clangd IDE integration.
-# Uses hedronvision/bazel-compile-commands-extractor.
-# See: https://github.com/hedronvision/bazel-compile-commands-extractor
-
-bazel run //tools:refresh_compile_commands
